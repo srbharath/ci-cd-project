58548782ac64f11380b372f367013bc644e6e15d
BharathSharath <103111105+BharathSharath@users.noreply.github.com>
2022-09-07 10:55:14 +0530

    Update backend-stack.sh

--- userdata/backend-stack.sh
+++ userdata/backend-stack.sh
@@ -36,17 +36,19 @@ systemctl enable memcached
 systemctl status memcached
 memcached -p 11211 -U 11111 -u memcached -d
 sleep 30
-yum install socat -y
-yum install wget -y
-wget https://www.rabbitmq.com/releases/rabbitmq-server/v3.6.10/rabbitmq-server-3.6.10-1.el7.noarch.rpm
-rpm --import https://www.rabbitmq.com/rabbitmq-release-signing-key.asc
-yum update
-rpm -Uvh rabbitmq-server-3.6.10-1.el7.noarch.rpm
-systemctl start rabbitmq-server
-systemctl enable rabbitmq-server
-systemctl status rabbitmq-server
-echo "[{rabbit, [{loopback_users, []}]}]." > /etc/rabbitmq/rabbitmq.config
-rabbitmqctl add_user test test
-rabbitmqctl set_user_tags test administrator
-systemctl restart rabbitmq-server
+sudo yum install epel-release -y
+sudo yum update -y
+sudo yum install wget -y
+sudo yum install socat -y
+sudo wget https://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_23.3.1-1~centos~7_amd64.rpm
+sudo yum -y install esl-erlang*.rpm
+sudo wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.19/rabbitmq-server-3.8.19-1.el7.noarch.rpm
+sudo yum -y install rabbitmq-server*.rpm 
+sudo systemctl start rabbitmq-server.service
+sudo systemctl enable rabbitmq-server
+sudo systemctl status rabbitmq-server
+sudo sh -c 'echo "[{rabbit, [{loopback_users, []}]}]." > /etc/rabbitmq/rabbitmq.config'
+sudo rabbitmqctl add_user test test
+sudo rabbitmqctl set_user_tags test administrator
+sudo systemctl restart rabbitmq-server
 
