ed6c59e755147284e666bf972e09d50d578133ad
BharathSharath <103111105+BharathSharath@users.noreply.github.com>
2022-09-07 10:54:23 +0530

    Update rabbitmq.sh

--- userdata/rabbitmq.sh
+++ userdata/rabbitmq.sh
@@ -5,7 +5,6 @@ sudo yum install wget -y
 sudo yum install socat -y
 sudo wget https://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_23.3.1-1~centos~7_amd64.rpm
 sudo yum -y install esl-erlang*.rpm
-sudo erl
 sudo wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.19/rabbitmq-server-3.8.19-1.el7.noarch.rpm
 sudo yum -y install rabbitmq-server*.rpm 
 sudo systemctl start rabbitmq-server.service
